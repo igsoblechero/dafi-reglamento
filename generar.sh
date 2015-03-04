c24364904b5db7bc70d0767f961a2450d3e32b37
Ignacio García Soblechero <igsoblechero@gmail.com>
2015-03-05 00:27:21 +0100

    Añadidos casos en generar.sh para disposiciones.

--- generar.sh
+++ generar.sh
@@ -5,6 +5,11 @@ sec=1
 cap=1
 tit=1
 
+ad=1
+tr=1
+der=1
+fin=1
+
 cat reglamento.list | while read file
 do
 	case $file in
@@ -37,6 +42,34 @@ do
 			let tit=tit+1
 			let cap=1
 			let sec=1
+		;;
+		
+		*.ad )
+			printf "#### Disposición adicional $adª. "
+			cat $file 2> /dev/null || echo $file
+			echo
+			let ad=ad+1
+		;;
+		
+		*.tr )
+			printf "#### Disposición transitoria $trª. "
+			cat $file 2> /dev/null || echo $file
+			echo
+			let tr=tr+1
+		;;
+		
+		*.der )
+			printf "#### Disposición derogatoria $derª. "
+			cat $file 2> /dev/null || echo $file
+			echo
+			let der=der+1
+		;;
+		
+		*.fin )
+			printf "#### Disposición final $finª. "
+			cat $file 2> /dev/null || echo $file
+			echo
+			let fin=fin+1
 		;;
 	esac
 done > reglamento.md
